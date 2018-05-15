09d3b472e667e1c3edf9c7e88fb2157471944fe1
gleb.streltsov <gleb.streltsov.4by@gmail.com>
2018-05-16 02:15:46 +0300

    update course work content

---
+++ addressAdder_inst.vhd
@@ -0,0 +1,5 @@
+addressAdder_inst : addressAdder PORT MAP (
+		dataa	 => dataa_sig,
+		datab	 => datab_sig,
+		result	 => result_sig
+	);
---
+++ stack_address_sub_inst.vhd
@@ -0,0 +1,4 @@
+stack_address_sub_inst : stack_address_sub PORT MAP (
+		dataa	 => dataa_sig,
+		result	 => result_sig
+	);
